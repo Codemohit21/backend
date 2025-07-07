d40893882d306fbcb8c8640084953a8cac8cfa8a
Codemohit21 <mkg7480081520@gmail.com>
2025-07-08 02:29:03 +0530

    utils code construction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv';
 import connectdb from './db/index.js';
+import { app } from './app.js';
 import mongoose from 'mongoose';
 import { DB_NAME } from './constant.js';
 
@@ -15,7 +16,22 @@ dotenv.config({
 
 
 
-connectdb();
+connectdb()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERRR CONNECTING TO SERVER: ",error);
+         throw error
+        })
+    app.listen(process.env.PORT || 8000,()=>{
+       console.log(`server is running at port : ${process.env.PORT}`);
+
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED", err);
+});
+
 
 
 
